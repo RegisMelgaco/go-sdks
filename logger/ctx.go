e87dd5c88f11fd410ab0f7931c42a8263eb6dc2b
Régis Melgaço de Andrade <regis.melgaco@gmail.com>
2022-11-29 21:28:29 -0300

    feat: logger.FromContext

--- logger/ctx.go
+++ logger/ctx.go
@@ -11,3 +11,12 @@ type loggerCtxKey struct{}
 func AddToCtx(ctx context.Context, logger *zap.Logger) context.Context {
 	return context.WithValue(ctx, loggerCtxKey{}, logger)
 }
+
+func FromContext(ctx context.Context) *zap.Logger {
+	logger, ok := ctx.Value(loggerCtxKey{}).(*zap.Logger)
+	if !ok {
+		panic("failed to retrieve logger from context")
+	}
+
+	return logger
+}
