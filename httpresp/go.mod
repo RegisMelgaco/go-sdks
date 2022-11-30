eb272bf1792bea8f3246bcf0095c48c227779d51
Régis Melgaço de Andrade <regis.melgaco@gmail.com>
2022-11-29 21:22:35 -0300

    refactor: logger middleware with logger module

--- httpresp/go.mod
+++ httpresp/go.mod
@@ -2,9 +2,10 @@ module github.com/regismelgaco/go-sdks/httpresp
 
 go 1.18
 
-require github.com/regismelgaco/go-sdks/erring v0.0.0-20221127002404-4d857029b5be
+require github.com/regismelgaco/go-sdks/erring v0.0.0-20221127113222-947ccd31e2bf
 
 require (
+	github.com/regismelgaco/go-sdks/logger v0.0.0-20221130001832-6bee3bf25aea // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.23.0 // indirect
--- httpresp/loggingMiddleware.go
+++ httpresp/loggingMiddleware.go
@@ -1,18 +1,16 @@
 package httpresp
 
 import (
-	"context"
 	"net/http"
 
+	log "github.com/regismelgaco/go-sdks/logger"
 	"go.uber.org/zap"
 )
-
-type loggerCtxKey struct{}
 
 func Logger(logger *zap.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx := context.WithValue(r.Context(), loggerCtxKey{}, logger)
+			ctx := log.AddToCtx(r.Context(), logger)
 
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
